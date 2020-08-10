26ae12fc461538cd27166af2aaf7f34312372bbe
Dongin Kim <kdi4348@naver.com>
2020-08-11 00:51:59 +0900

    Daegudot springframework server first commit!
    1. to add RestController base code.
    2. to add Junit4 test code.

---
+++ src/test/java/com/kop/daegudot/web/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.kop.daegudot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void test_helloController() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+}
