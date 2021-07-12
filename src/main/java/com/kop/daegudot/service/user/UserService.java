79d572da704de8499c93a051b4a158b5ea2cbd99
HGYEONG <hyeony1070@gmail.com>
2021-07-12 17:40:38 +0900

    Modify Update part 1. seperate updateDto to nickname and password 2. add update function and dto by nickname 3. add update function and dto by password

--- src/main/java/com/kop/daegudot/service/user/UserService.java
+++ src/main/java/com/kop/daegudot/service/user/UserService.java
@@ -4,10 +4,7 @@ import com.kop.daegudot.domain.user.User;
 import com.kop.daegudot.domain.user.UserRepository;
 import com.kop.daegudot.web.JWT.JwtTokenProvider;
 import com.kop.daegudot.web.dto.TokenResponseDto;
-import com.kop.daegudot.web.dto.user.UserLoginDto;
-import com.kop.daegudot.web.dto.user.UserResponseDto;
-import com.kop.daegudot.web.dto.user.UserRegisterDto;
-import com.kop.daegudot.web.dto.user.UserUpdateDto;
+import com.kop.daegudot.web.dto.user.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -43,10 +40,18 @@ public class UserService {
     }
 
     // UPDATE
-    public Long updateById(long userId, UserUpdateDto userUpdateDto){
+    public Long updateNicknameById(long userId, UserUpdateNicknameDto userUpdateNicknameDto){
         User user = mUserRepository.findById(userId)
                 .orElseThrow(()->new IllegalArgumentException("There is no places id = " + userId));
-        user.update(userUpdateDto.getNickname(), userUpdateDto.getPassword());
+        user.nicknameUpdate(userUpdateNicknameDto.getNickname());
+        mUserRepository.save(user);
+        return userId;
+    }
+
+    public Long updatePasswordById(long userId, UserUpdatePasswordDto userUpdatePasswordDto){
+        User user = mUserRepository.findById(userId)
+                .orElseThrow(()->new IllegalArgumentException("There is no places id = " + userId));
+        user.passwordUpdate(userUpdatePasswordDto.getPassword());
         mUserRepository.save(user);
         return userId;
     }
