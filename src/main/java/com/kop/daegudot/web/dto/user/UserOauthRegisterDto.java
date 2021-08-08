7c7d75c6aa94031d4739cc2da6c4b3cdd736852d
HGYEONG <hyeony1070@gmail.com>
2021-08-09 01:41:32 +0900

    add @NoArgsConstructor in UserOauthRegisterDto

--- src/main/java/com/kop/daegudot/web/dto/user/UserOauthRegisterDto.java
+++ src/main/java/com/kop/daegudot/web/dto/user/UserOauthRegisterDto.java
@@ -1,8 +1,10 @@
 package com.kop.daegudot.web.dto.user;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class UserOauthRegisterDto {
     private String oauthToken;
 
